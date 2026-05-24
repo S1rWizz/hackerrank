7985df1745112666c76f08d10f7f55e8b69596b0
SirW1zz <justinjovenm@gmail.com>
2026-05-24 20:53:52 +0530

    Started neetcode

---
+++ duplicates.py
@@ -0,0 +1,10 @@
+# https://neetcode.io/problems/duplicate-integer/history?list=neetcode150&submissionIndex=0
+
+
+class Solution:
+    
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        return len(nums)!=len(set(nums))
+        
+ex=Solution()
+print(ex.hasDuplicate([1,2,3,4,5]))
